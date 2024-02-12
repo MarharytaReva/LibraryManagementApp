ca7e880a16813f4b35d9b0492ee7108e67fb9837
Marharyta Reva <reva.marharyta@gmail.com>
2024-02-12 17:01:31 -0500

    Author

--- src/Models/Author.java
+++ src/Models/Author.java
@@ -1,2 +1,34 @@
-package Models;public class Author {
+package Models;
+
+public class Author {
+    private int authorId;
+    private String fullname;
+
+    public Author()
+    {
+        fullname = "";
+    }
+    public Author(String fullname) {
+        this.fullname = fullname;
+    }
+    public Author(int authorId, String fullname) {
+        this.authorId = authorId;
+        this.fullname = fullname;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
 }
